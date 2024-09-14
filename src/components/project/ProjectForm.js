1b64ca3056cf93b5ae425901157f665854c02194
Hscaio <caiosoareslt@outlook.com>
2024-09-14 11:04:15 -0300

    Implementado Home, Form para NewProject e falso backend

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,47 @@
+import {useState, useEffect} from 'react'
+
+import styles from './ProjectForm.module.css'
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+
+function ProjectForm({btnText}){
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((resp) => resp.json())
+        .then((data) => setCategories(data))
+        .catch((err) => console.log(err))
+    },[])
+
+    return(
+        <form className={styles.from}>
+            <Input 
+                type="text" 
+                placeholder="Insira o nome do projeto" 
+                name="name" 
+                text="Nome do projeto"
+            />
+            <Input 
+                type="number" 
+                placeholder="Insira o orçamento total" 
+                name="budget" 
+                text="Orçamento do projeto"
+            />   
+            <Select
+                name="category_id"
+                text="Selecione a categoria"
+                options={categories}
+            />    
+            <SubmitButton text={btnText}/>
+        </form>
+    )
+}
+
+export default ProjectForm
